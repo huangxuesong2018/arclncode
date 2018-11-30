8b9858d190d98ef4a271500ded67b7c2081bd86c
huangxuesong <106410277@qq.com>
2018-11-30 15:47:49 +0800

    edit clientZKDSClientApplication -> ClientZKDSClientApplication

--- microservices-project1/spring-cloud-project/spring-cloud-service-discovery-client/src/main/java/com/gupao/micro/service/spring/cloud/ds/client/ClientZKDSClientApplication.java
+++ microservices-project1/spring-cloud-project/spring-cloud-service-discovery-client/src/main/java/com/gupao/micro/service/spring/cloud/ds/client/ClientZKDSClientApplication.java
@@ -1,13 +1,13 @@
-package com.gupao.micro.service.spring.cloud.ds;
+package com.gupao.micro.service.spring.cloud.ds.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient//尽可能使用@EnableDiscoveryClient
-public class clientZKDSClientApplication {
+@EnableDiscoveryClient
+public class ClientZKDSClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(clientZKDSClientApplication.class,args);
+        SpringApplication.run(ClientZKDSClientApplication.class,args);
     }
 }
