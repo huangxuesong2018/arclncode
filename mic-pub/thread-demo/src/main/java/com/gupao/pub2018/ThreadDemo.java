e1fb8ddc9caa23f1f9c7bc4e606c70a923131038
huangxuesong <106410277@qq.com>
2019-01-28 14:04:45 +0800

    Thread demo

---
+++ mic-pub/thread-demo/src/main/java/com/gupao/pub2018/ThreadDemo.java
@@ -0,0 +1,19 @@
+package com.gupao.pub2018;
+public class ThreadDemo {
+    /* 加上 volatile 后，可以看到
+       0x0000000002c10acd: lock add dword ptr [rsp],0h  ;*putstatic instance
+                      ; - com.gupao.pub2018.ThreadDemo::getInstance@13 (line 7)
+    */
+    private static volatile ThreadDemo instance = null;
+
+    public static ThreadDemo getInstance(){
+        if(instance == null){
+            instance = new ThreadDemo();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        ThreadDemo.getInstance();
+    }
+}
